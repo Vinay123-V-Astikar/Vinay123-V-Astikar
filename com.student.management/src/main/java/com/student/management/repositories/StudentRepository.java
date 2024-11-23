baf865e2c4046d8946841bdae05216d2d22e96b2
Vinay123-V-Astikar <96983746+Vinay123-V-Astikar@users.noreply.github.com>
2024-11-23 16:48:52 +0530

    Delete com.student.management directory

--- com.student.management/src/main/java/com/student/management/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.management.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.student.management.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Integer>{
-
-}
